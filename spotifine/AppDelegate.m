0a735ce36c1003ad61503dd21f94afa2eb3347a3
Onur Baykal <theaob@gmail.com>
2014-12-03 20:17:15 +0200

    added testflight key

--- spotifine/AppDelegate.m
+++ spotifine/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TestFlight.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [TestFlight takeOff:@"861326bd-e0d8-4c7b-8688-29c5096616da"];
     return YES;
 }
 
